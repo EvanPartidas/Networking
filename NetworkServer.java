7d1d1693d76da15ec59e41a3c5bc152b409ea09c
Evan Partidas <evanpartidas@gmail.com>
2020-09-16 16:19:10 -0400

    Init

---
+++ NetworkServer.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class NetworkServer
+{
+	public static void main(String[] args) throws Exception
+	{
+		System.out.println("Opening Socket");
+		ServerSocket server = new ServerSocket(17395);
+		System.out.println("Opened");
+		while(true)
+		{
+			try{
+			Socket client = server.accept();
+			System.out.println("Client Accepted");
+			DataOutputStream out = new DataOutputStream(new BufferedOutputStream(client.getOutputStream()));
+			DataInputStream in = new DataInputStream(new BufferedInputStream(client.getInputStream()));
+			String str = in.readUTF();
+			System.out.printf("%s said %s",client.getRemoteSocketAddress(),str);
+			out.writeUTF("Hello, thank you for trying out networking! the flag is: zetactf{y0u_l34RnEd_t0_n3Tw0rk_1337}");
+			out.flush();
+			out.close();
+			in.close();
+			client.close();
+			System.out.println("Client Disconnected");
+			}catch(Exception e){e.printStackTrace();}
+		}
+	}
+}
